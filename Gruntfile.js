bf9bfc375a13cabe97c35c5546345ca95f885beb
Zoran Dori <zorandori4444@gmail.com>
2023-02-12 00:14:08 +0100

    build: Update devDependencies, add stylelint
    Change-Id: Iff57ed98a74ac3dc5715fcd071ffbde5ab27be6a

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,14 +1,16 @@
-/* eslint-env node */
-
+/* eslint-env node, es6 */
 module.exports = function ( grunt ) {
 	var conf = grunt.file.readJSON( 'extension.json' );
 
 	grunt.loadNpmTasks( 'grunt-banana-checker' );
 	grunt.loadNpmTasks( 'grunt-eslint' );
+	grunt.loadNpmTasks( 'grunt-stylelint' );
 
 	grunt.initConfig( {
+		banana: conf.MessagesDirs,
 		eslint: {
 			options: {
+				extensions: [ '.js', '.json' ],
 				cache: true
 			},
 			all: [
@@ -18,9 +20,16 @@ module.exports = function ( grunt ) {
 				'!resources/ext.biblioPlus.qtip/ext.biblioPlus.qtip.min.js'
 			]
 		},
-		banana: conf.MessagesDirs
+		stylelint: {
+			all: [
+				'**/*.{css,less}',
+				'!node_modules/**',
+				'!vendor/**',
+				'!resources/ext.biblioPlus.qtip/ext.biblioPlus.qtip.min.css'
+			]
+		}
 	} );
 
-	grunt.registerTask( 'test', [ 'eslint', 'banana' ] );
+	grunt.registerTask( 'test', [ 'eslint', 'banana', 'stylelint' ] );
 	grunt.registerTask( 'default', 'test' );
 };
